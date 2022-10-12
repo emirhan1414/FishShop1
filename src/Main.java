f334795d7998de38ef020750c2b57dc6cd9dbaf8
emirhan1414 <113902435+emirhan1414@users.noreply.github.com>
2022-10-12 11:53:49 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+
+public class Main {
+	public static void main(String[] args) {
+		Fish fs=new Fish();
+		Rod rod=new Rod();		
+		Bait bait=new Bait();
+		Aquarium aq1=new Aquarium();
+		
+		fs.setName("DENÝZ ALASI");
+	    fs.setFeatures("Vücut yanlardan yassýlaþmýþ, füze þeklinde zeytin yeþili renkli olup yanal çizginin üstü beyaz halkalarla çevrili kýrmýzý beneklidir. Kýrmýzý beneklerin beyaz halkalý ve siyah beneklerin yan çizginin altýna inmesi ile diðer alt türlerden ayrýlýr.");
+		
+	    rod.setName("Discovery");
+		rod.setFeatures("Discovery Baþlangýç Seti Yeni Dft Racer 360CM 100-150GR Charisma Silver Misinalý ");
+	
+		bait.setName("Sera Insect");
+		bait.setFeatures("Sera Insect Nature Balýk Yemi 250 ML");
+		aq1.setAquarium("Fluval");
+		aq1.setFeatures("Fluval Flex Akvaryum 34Lt Siyah");
+			System.out.println(rod.getName()+" : "+rod.getFeatures());
+		System.out.println(fs.getName()+" : "+fs.getFeatures());
+		System.out.println(bait.getName()+" : "+bait.getFeatures());
+		System.out.println(aq1.getAquarium()+" : "+aq1.getFeatures());
+		
+        
+	
+	
+	}
+}
